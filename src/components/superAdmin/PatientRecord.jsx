07d9afd919e91551bd784b260ccbda083055e7e0
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2025-10-25 15:08:05 +0530

    table css style

--- src/components/superAdmin/PatientRecord.jsx
+++ src/components/superAdmin/PatientRecord.jsx
@@ -55,11 +55,7 @@ export function PatientRecords() {
             </div>
 
 
-            <div style={{
-                marginTop: '10px',
-                // alignContent: 'center',
-                // alignItems: 'center'
-            }}>
+            <div style={{ marginTop: '10px' }}>
                 <div className="patientRecordsHeadings">
                     <p>Patient ID</p>
                     <p>Name</p>
@@ -68,68 +64,53 @@ export function PatientRecords() {
                     <p>Doctor</p>
                     <p>Date</p>
                 </div>
-                {/* <div className="patientRecordBody">
-                {isProcessing && (
-                    <span style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        flexDirection: 'column',
-                        padding: '50px 0'
-                    }}>
-                        <Circles height="40" width="40" color="#4f46e5" ariaLabel="loading" />
-                        <br />Loading...
-                    </span>
-                )}
-                {error && (
-                    <h4 style={{
-                        color: 'red',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        padding: '50px 0'
-                    }}>{error}</h4>
-                )} */}
+                <div className="patientRecordBody">
+                    {isProcessing && (
+                        <span style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            flexDirection: 'column',
+                            padding: '50px 0'
+                        }}>
+                            <Circles height="40" width="40" color="#4f46e5" ariaLabel="loading" />
+                            <br />Loading...
+                        </span>
+                    )}
 
-                {!isProcessing && !error && Array.isArray(filterPatient) && filterPatient.length > 0 && (
-                    // <div style={{
+                    {error && (
+                        <h4 style={{
+                            color: 'red',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            padding: '50px 0'
+                        }}>{error}</h4>
+                    )}
 
-                    // }}>
-                    <>
-                        {filterPatient.map((patient, i) => (
-                            <div key={i}
-                                style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    // height: '75px',
-                                    backgroundColor: 'white',
-                                    border: '1px solid lightgray',
-                                    // padding: '20px',
-                                    cursor: 'pointer',
+                    {!isProcessing && !error && Array.isArray(filterPatient) && filterPatient.length > 0 && (
+                        <>
+                            {filterPatient.map((patient, i) => (
+                                <div key={i}>
+                                    <p>{patient.uid}</p>
+                                    <p>{patient.name}</p>
+                                    <p>{patient.age}</p>
+                                    <p>{patient?.hospitalId?.name || "N/A"}</p>
+                                    <p>{patient?.doctorId?.name || "N/A"}</p>
+                                    <p>{patient.date || "1990-05-15"}</p>
+                                </div>
+                            ))}
+                        </>
+                    )}
 
-                                    // gap: '180px'
-
-                                }}
-                                // className="patientRecordsHeadings"
-                            >
-                                <p style={{textAlign:'center', background:'red'}}>{patient.uid}</p>
-                                <p style={{textAlign:'center', background:'red'}}>{patient.name}</p>
-                                <p style={{textAlign:'center', background:'red'}}>{patient.age}</p>
-                                <p style={{textAlign:'center', background:'red'}}>{patient?.hospitalId?.name || "N/A"}</p>
-                                <p style={{textAlign:'center', background:'red'}}>{patient?.doctorId?.name || "N/A"}</p>
-                                <p style={{textAlign:'center', background:'red'}}>{"1990-05-15"}</p>
-                            </div>
-                        ))}
-                    </>
-                )}
-
-                {!isProcessing && !error && Array.isArray(filterPatient) && filterPatient.length === 0 && (
-                    <p
-                        style={{ textAlign: 'center', padding: '50px 0' }}
-                    >No Patient found</p>
-                )}
+                    {!isProcessing && !error && Array.isArray(filterPatient) && filterPatient.length === 0 && (
+                        <p style={{ textAlign: 'center', padding: '50px 0' }}>
+                            No Patient found
+                        </p>
+                    )}
+                </div>
             </div>
+
         </div>
-        // </div>
     )
 }
